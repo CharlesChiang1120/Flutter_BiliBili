08219ae816f5ce26f2538c07903dd2e5b9b4a0de
Charles Chiang <charleschiang2343@gmail.com>
2024-11-26 20:33:06 +0800

    Lesson1,Lesson2

---
+++ 03_demo.dart
@@ -0,0 +1,15 @@
+void main() {
+  // var str = "Hello Darttt";
+  // var myNum = 123;
+
+  // print(str);
+  // print(myNum);
+
+  // 字符串
+  String str = "Hello Darttt";
+  print(str);
+
+  // 整數類型
+  int myNum = 1234;
+  print(myNum);
+}
