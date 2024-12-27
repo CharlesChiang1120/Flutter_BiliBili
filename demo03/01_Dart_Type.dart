b886d085d7f053fadd07c798f188166e95a9280b
Charles Chiang <charleschiang2343@gmail.com>
2024-12-27 14:21:34 +0800

    demo03/01_Dart_Type.dart

--- demo03/01_Dart_Type.dart
+++ demo03/01_Dart_Type.dart
@@ -5,4 +5,10 @@ void main(List<String> args) {
 
   print(str1);
   print(str2);
+
+  dynamic variable = 42; // 現在是整數
+  print(variable); // 輸出 42
+
+  variable = "Hello"; // 現在是字串
+  print(variable); // 輸出 Hello
 }
