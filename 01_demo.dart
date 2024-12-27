9e8e7e2df2b41d9d9cd0ac3735322bb9e80b060f
Charles Chiang <charleschiang2343@gmail.com>
2024-12-27 15:24:46 +0800

    move to folder

--- 01_demo.dart
+++
@@ -1,8 +0,0 @@
-// main() {
-//   print("Hello Dart");
-// }
-
-// 表示 main 方法沒有返回值
-void main() {
-  print("Hello Dart");
-}
--- 04_demo.dart
+++
@@ -1,4 +0,0 @@
-void main(List<String> args) {
-  var str1 = "12341234";
-  print(str1);
-}
