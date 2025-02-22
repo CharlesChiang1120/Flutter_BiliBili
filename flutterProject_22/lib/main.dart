427d3d9599ea32a4533bda9540c04274b8f5e5fe
Charles Chiang <charleschiang2343@gmail.com>
2025-02-22 13:37:24 +0800

    22 Flutter 圖片組件 - Image、本地圖片、遠程圖片、圖片剪切 (28:50)

--- flutterProject_22/lib/main.dart
+++ flutterProject_22/lib/main.dart
@@ -1,125 +1,107 @@
+// 22 Flutter 圖片組件 - Image、本地圖片、遠程圖片、圖片剪切 (28:50)
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Hello Flutter"),
+      ),
+      body: Column(children: [
+        MyApp(),
+        SizedBox(
+          height: 20,
+        ),
+        Circular(),
+        SizedBox(
+          height: 20,
+        ),
+        ClipImage(),
+        SizedBox(
+          height: 20,
+        ),
+        LocalImage()
+      ]),
+    ),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return Center(
+        child: Container(
+      margin: EdgeInsets.fromLTRB(0, 40, 0, 0),
+      // alignment: Alignment.centerLeft,
+      width: 150,
+      height: 150,
+      decoration: BoxDecoration(
+        color: Colors.yellow,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      child: Image.network(
+        "https://images.pexels.com/photos/29195423/pexels-photo-29195423.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        // scale: 2,
+        // alignment: Alignment.centerLeft,
+        fit: BoxFit.cover,
+        // repeat: ImageRepeat.repeat,
+        // width: 100,
+        // height: 100,
+      ),
+    ));
+  }
+}
+
+class Circular extends StatelessWidget {
+  const Circular({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      height: 150,
+      decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(75),
+          image: DecorationImage(
+              image: NetworkImage(
+                  "https://images.pexels.com/photos/29195423/pexels-photo-29195423.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+              fit: BoxFit.cover)),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class ClipImage extends StatelessWidget {
+  const ClipImage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return ClipOval(
+      child: Image.network(
+        "https://images.pexels.com/photos/29195423/pexels-photo-29195423.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        width: 150,
+        height: 150,
+        alignment: Alignment.bottomCenter,
+        fit: BoxFit.cover,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class LocalImage extends StatelessWidget {
+  const LocalImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 150,
+      height: 150,
+      // decoration: const BoxDecoration(
+      //   color: Colors.amber,
+      // ),
+      child: Image.asset("images/lebron.jpeg"),
     );
   }
 }
